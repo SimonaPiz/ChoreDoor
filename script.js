06e92fd39da07665772b578581f5410753b41fd4
Simona Pizio <simona.pizio@gmail.com>
2023-11-20 15:53:04 +0100

    create program #1 - Set iniatial game round

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ let startButton = document.getElementById('start');
 let botPath = 'robot.svg';
 let beachPath = 'beach.svg';
 let spacePath = 'space.svg';
-let closedPath = 'closed_door.svg';
+let closedPath = 'closed-door.svg';
 const path = './img/';
+
+// Set initial variables
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
+
+// Start a game round
+const startRound = () => {
+  console.log('Game starts!');
+  door1.src = path + closedPath;
+  door2.src = path + closedPath;
+  door3.src = path + closedPath;
+
+  numClosedDoors = 3;
+  currentlyPlaying = true;
+  startButton.innerHTML = 'Good Luck!';
+}
+
+startRound();
