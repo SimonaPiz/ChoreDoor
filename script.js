d555670dd1d3c238a1810501a7f6530407af13ca
Simona Pizio <simona.pizio@gmail.com>
2023-11-20 15:33:08 +0100

    create program #1 - Access HTML elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Access HTML elements
+let door1 = document.getElementById('door1');
+let door2 = document.getElementById('door2');
+let door3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
