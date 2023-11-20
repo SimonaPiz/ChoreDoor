1cbfe769e6dbe632e098451f3a630b93fe23e6a5
Simona Pizio <simona.pizio@gmail.com>
2023-11-20 15:37:11 +0100

    create program #1 - Set image paths

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let door1 = document.getElementById('door1');
 let door2 = document.getElementById('door2');
 let door3 = document.getElementById('door3');
 let startButton = document.getElementById('start');
+
+// set images path
+let botPath = 'robot.svg';
+let beachPath = 'beach.svg';
+let spacePath = 'space.svg';
+let closedPath = 'closed_door.svg';
+const path = './img/';
