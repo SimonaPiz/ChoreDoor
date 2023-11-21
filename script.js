7c3c571af303e95217f896945d158ac8f69dd461
Simona Pizio <simona.pizio@gmail.com>
2023-11-21 10:28:20 +0100

    create program #1 - add click event on button to reset game

--- script.js
+++ script.js
@@ -19,9 +19,10 @@ let currentlyPlaying = true;
 
 // Load initial random images behind the doors
 const randomChoreDoorGenerator = () => {
-  console.log('Random Chore door is called');
   let choreDoor = Math.floor(Math.random() *3);
-  console.log(choreDoor);
+  areOpenDoors[1] = false;
+  areOpenDoors[2] = false;
+  areOpenDoors[3] = false;
 
   if (choreDoor === 0) {
     imgOpenDoors[1] = botPath;
@@ -36,13 +37,11 @@ const randomChoreDoorGenerator = () => {
     imgOpenDoors[1] = beachPath;
     imgOpenDoors[2] = spacePath;
   }
-  console.log(imgOpenDoors[1],imgOpenDoors[2],imgOpenDoors[3]);
 
 }
 
 // Start a game round
 const startRound = () => {
-  console.log('Game starts!');
   door1.src = path + closedPath;
   door2.src = path + closedPath;
   door3.src = path + closedPath;
@@ -58,27 +57,29 @@ startRound();
 // Set click event
 const clickEvent = (door) => {
   let doorNum = Number(door.id.slice(-1));
-  console.log('-Click door',doorNum);
+
   if (currentlyPlaying && !areOpenDoors[doorNum]) {
     areOpenDoors[doorNum] = true;
     door.src = path + imgOpenDoors[doorNum];
     numClosedDoors--;
-    console.log('open door')
     
     //check door
     if (numClosedDoors === 0) {
-      console.log('game ends, you win!');
       startButton.innerHTML = 'You win! Play again?';
       currentlyPlaying = false;
     } else if (door.src.includes('bot')) {
-      console.log('It\'s bot, you lose');
       startButton.innerHTML = 'Game over! Play again?';
       currentlyPlaying = false;
     }
-  } else console.log('the door is already open')
+  }
 }
 
 // Add click event on doors
 door1.addEventListener('click', (ev) => clickEvent(ev.target));
 door2.addEventListener('click', (ev) => clickEvent(ev.target));
 door3.addEventListener('click', (ev) => clickEvent(ev.target));
+
+// Add click event on button
+startButton.addEventListener('click', (ev) => {
+  startRound();
+});
